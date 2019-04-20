a6d0770f6f7d1b4b41458f51e269ec12bbb29b77
Jim Hough <teachlife@outlook.com>
2019-04-20 23:01:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array<<element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-
+  array.pop
+  array
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array[-1]
 end
